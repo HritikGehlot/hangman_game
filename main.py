c6ceda16315a04c9880feda2f772d303172bbe6a
Hritik Gehlot <hgehlot1007@gmail.com>
2024-09-17 14:36:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import random
+import hangman_art
+import hangman_word_list_of_animals as hgwl 
+import rules
+random_letter = random.choice(hgwl.word_list) 
+# print(random_letter)
+lives = 6
+correct_letter = []
+game_over = False
+
+hangman_art.welcome()
+print(rules.rule)
+print(f"This word contains {len(random_letter)} letters")
+while not game_over:
+
+    print(hangman_art.stages[-lives-1])
+    print(f"Total no of lives remaining: {lives}")
+    
+    user_input = input("Guess a letter: ").lower()
+     
+    display = ""
+
+    
+    for letter in random_letter:
+        if letter == user_input:
+            # print("Right")
+            display += letter
+            correct_letter.append(letter)
+            # display -= "_"
+        elif letter in correct_letter:
+            display += letter    
+            
+  
+        else:
+            # print("Wrong")  
+               
+            display += "_"
+
+    if not user_input in display:
+        lives-=1
+
+    if user_input in display:
+        print("Please enter another letter")              
+    
+    if not "_" in display:
+        game_over = True
+        hangman_art.won()
+    if lives == 0:
+        game_over = True
+        print(hangman_art.stages[-1]) 
+        hangman_art.lose()
+        print("Game over hangman died")   
+
+
+    print(display)
---
+++ rules.py
@@ -0,0 +1,4 @@
+rule = f"""\tRules
+1:You have 6 lives.
+2:It is name of a animal.
+"""
